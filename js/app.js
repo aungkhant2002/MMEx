3f3d2cbae535003d3978083d9064572e9ad67765
AungKhant <aung.khant@ucsy.edu.mm>
2021-12-23 10:50:35 +0630

    calculation process

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+let input = document.getElementById("input");
 let from = document.getElementById("from");
 let to = document.getElementById("to");
+let result = document.getElementById("result");
 
 function createOption(x, y, z) {
     let o = document.createElement("option");
@@ -17,3 +19,25 @@ for (x in data.rates) {
     createOption(from, x, data.rates[x]);
     createOption(to, x, data.rates[x]);
 }
+
+document.getElementById("calc").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // get state
+    let x = input.value;
+    let y = from.value;
+    let z = to.value;
+
+    console.log(x, y, z);
+
+    // process
+    let first = x * y;
+    let second = first/z;
+
+    // set state
+    result.innerHTML = second.toFixed(2);
+    input.value = '';
+    input.focus();
+    from.value = '';
+    to.value = '1';
+})
